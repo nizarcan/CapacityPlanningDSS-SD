8326be6ba09c80c43d8be8fa074bf228cded15af
nizarcan <nizarcan@github.com>
2020-01-17 10:52:42 +0300

    rearrangements
    updated some lines to fetch the file from outside of the source

---
+++ input_generator/constants.py
@@ -0,0 +1,10 @@
+"""
+TC MANGO
+Systems Design Project Constants & File Paths
+"""
+
+bom_path = "C:/sl_data/bom.xlsx"
+bom_sheet_name = "2100 Sonrası BOM"
+times_path = "C:/sl_data/times.xlsx"
+times_sheet_name = "DÜZLİST"
+tbd_path = "C:/sl_data/tbd.xlsx"
--- input_generator/frontend.py
+++ input_generator/frontend.py
@@ -1,10 +1,11 @@
-from app_backend import *
+from input_generator.app_backend import *
+import input_generator.constants as constants
 
 # BOM dosyasını okuma aşaması
-bom_df = read_excel_sheet("test_app/bom.xlsx", "2100 Sonrası BOM")
-bom_df = arrange_df(bom_df, [0, 3, 2, 5, 4], "bom", "test_app/silinecekler.xlsx")
+bom_df = read_excel_sheet(constants.bom_path, constants.bom_sheet_name)
+bom_df = arrange_df(bom_df, [0, 3, 2, 5, 4], "bom", constants.tbd_path)
 
-times_df = read_excel_sheet("test_app/times.xlsx", "DÜZLİST")
+times_df = read_excel_sheet(constants.times_path, constants.times_sheet_name)
 times_df = arrange_df(times_df, [3, 25, 29, 30], "times")
 times_df["station"] = format_machine_names(times_df, "station")
 merged_df = merge_times_and_bom(bom_df, times_df)
